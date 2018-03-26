310ae7571398c761077fe707a7a3ebb495944990
Daniel Schulz <daniel.schulz@ocono.de>
2018-03-26 17:19:43 +0200

    Implement

---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"flag"
+	"math/rand"
+	"time"
+	"strconv"
+)
+
+type Service struct {
+	Max int
+}
+
+func (s Service) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UnixNano())
+	ms := rand.Intn(s.Max - 1)
+	time.Sleep(time.Duration(ms) * time.Millisecond)
+	rw.Write([]byte(strconv.Itoa(ms)))
+}
+
+func main() {
+	var max int
+	flag.IntVar(&max, "max", 1, "The max sleep time in ms")
+
+	flag.Parse()
+
+	http.Handle("/", Service{Max: max})
+
+	http.ListenAndServe(":80", nil)
+}
